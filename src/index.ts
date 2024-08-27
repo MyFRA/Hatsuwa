50d3c6e1fff76a78aee995025a9bd517f36ae1b0
MyFRA <tomyntapss@gmail.com>
2024-08-27 19:55:52 +0700

    chore: add swagger ui

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { Request, Response } from "express";
 
 const express = require("express");
 const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3300;
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
